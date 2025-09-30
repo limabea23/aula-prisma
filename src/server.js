bf9e4fe8d9c690b6fc73b63d8342f7d72a517685
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-09-30 10:46:21 -0300

    feat: atualizações/alterações

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express"; //executa o projeto
 import { config } from "dotenv";
 
 import router from "./routes/index.routes.js";
+import movieRouter from './routes/movie.routes.js';
 
 config(); // Carrega variáveis de ambiente do arquivo .env
 const port = process.env.PORT || 5081;
@@ -13,6 +14,8 @@ app.use(express.json()); // Parse de JSON
 
 // Rota base para verificar se o servidor está rodando
 app.get("/", router);
+
+app.use('/movies', movieRouter);
 
 // Iniciar o servidor
 app.listen(port, () => {
